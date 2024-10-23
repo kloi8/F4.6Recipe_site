cd2c80683624b4e1eca949eda84b5a05aeaaa37e
kloi8 <152299798+kloi8@users.noreply.github.com>
2024-10-23 18:13:23 +0300

    Add files via upload

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -4,32 +4,51 @@ import axios from "axios";
 import { useParams } from 'react-router-dom';
 // import { withRouter } from "react-router";
 
-
-const Category = () => {
-    const category = useParams();
-    const [categories, setCategories] = useState([])
+const [data, setData] = useState([])
+// const Category = () => {
+//     const category = useParams();
+//     const [categories, setCategories] = useState([]) 
     // const [loading, setLoading] = useState(true)
   
+    // useEffect(() => {
+    //   axios.get("http://127.0.0.1:8000/api/recipe/category")
+    //   .then(res => {
+    //     console.log(res.data)
+    //     setCategories(res.data)
+    //     // setLoading(false)
+    //   })
+    //   .catch(err => console.log(err.message))
+    // }, [])
+
+
     useEffect(() => {
-      axios.get("http://127.0.0.1:8000/recipe/category")
-      .then(res => {
-        console.log(res.data)
-        setCategories(res.data)
-        // setLoading(false)
-      })
-      .catch(err => console.log(err.message))
-    }, [])
+      fetch("http://localhost:8000/api/recipe/category")
+        .then(data => data.json())
+        .then(response => setData(response))
+        .catch(error => console.log(error))
+    }, []);
 
-    return(
-        <div className="container mx-auto mt-8 mb-8 px-4 flex flex-wrap justify-evenly">
-            <ul className='category'> 
 
-        {categories.map(category =>  ( 
-            <Link to={'recipe/category'} className='text-red-200 hover:text-green-500'>{category.title}</Link> 
-        ))}
-     </ul>
-        </div>
-    )
-}
+      return(
+          // <div className="container mx-auto mt-8 mb-8 px-4 flex flex-wrap justify-evenly">
+          //     <ul className='category'> 
+
+          {category.map(category =>  { 
+            return (
+               <div>{ category }</div>
+            )
+                   
+            })
+          }
+          
+          // </ul>
+          // </div>
+      )
+
+
+    //  </ul>
+    //     </div>
+//     )
+// } }
 
 export default Category
