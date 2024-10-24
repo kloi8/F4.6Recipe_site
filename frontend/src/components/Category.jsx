947e5bc2605925f0cbc9c47a92dce8616f776385
kloi8 <152299798+kloi8@users.noreply.github.com>
2024-10-24 12:11:47 +0300

    Add files via upload

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -4,51 +4,29 @@ import axios from "axios";
 import { useParams } from 'react-router-dom';
 // import { withRouter } from "react-router";
 
-const [data, setData] = useState([])
-// const Category = () => {
-//     const category = useParams();
-//     const [categories, setCategories] = useState([]) 
-    // const [loading, setLoading] = useState(true)
-  
-    // useEffect(() => {
-    //   axios.get("http://127.0.0.1:8000/api/recipe/category")
-    //   .then(res => {
-    //     console.log(res.data)
-    //     setCategories(res.data)
-    //     // setLoading(false)
-    //   })
-    //   .catch(err => console.log(err.message))
-    // }, [])
+const Categories = () => {
+  const [data, setData] = useState([])
+
+      useEffect(() => {
+        fetch("http://localhost:8000/api/recipe/category")
+          .then(data => data.json())
+          .then(response => setData(response))
+          .catch(error => console.log(error))
+      }, []);
 
 
-    useEffect(() => {
-      fetch("http://localhost:8000/api/recipe/category")
-        .then(data => data.json())
-        .then(response => setData(response))
-        .catch(error => console.log(error))
-    }, []);
+        return(
 
+            {data.map((category, index) => ( // Использование index в качестве ключа (если нет уникального ID)
+          <div key={index}>{category}</div>
+        ))}
 
-      return(
-          // <div className="container mx-auto mt-8 mb-8 px-4 flex flex-wrap justify-evenly">
-          //     <ul className='category'> 
-
-          {category.map(category =>  { 
-            return (
-               <div>{ category }</div>
-            )
-                   
-            })
-          }
-          
-          // </ul>
-          // </div>
-      )
-
-
-    //  </ul>
-    //     </div>
-//     )
-// } }
+        )
+}
 
 export default Category
+
+
+
+
+
